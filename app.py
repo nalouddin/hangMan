1386f72d208cbc34cb4cf0271b030ff6c94c7897
nalouddin <nalouddin@gmail.com>
2025-12-11 11:14:59 +0500

    hangman dasturi tugadi

--- app.py
+++ app.py
@@ -17,8 +17,11 @@ game_over = False
 correct_letters = []
 
 while not game_over:
+    print(f"****************************{lives}/6 LIVES LEFT**************************************")
     guess = input("Guess a letter: ").lower()
 
+    if guess in correct_letters:
+        print(f"You've already guessed {guess}")
 
     display = ""
 
@@ -34,12 +37,14 @@ while not game_over:
 
     if guess not in chosen_word:
         lives -= 1
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+
         if lives == 0:
             game_over = True
-            print("You lose.")
+            print(f"**************************IT WAS {chosen_word} YOU LOSE***********************")
 
     if "_" not in display:
         game_over = True
-        print("You win!")
+        print("**************************YOU WIN***********************")
 
     print(stages[lives])
