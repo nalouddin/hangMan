331581b8c1bd324a55dafd24875327a20506cdf1
nalouddin <nalouddin@gmail.com>
2025-12-11 10:12:56 +0500

    randam yordamida listdagi so'zlarni ajratib olamiz

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
-word_list = ["ardvark", "baboon", "camel"]
+import random
 
+word_list = ["ardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+guess = input("Guess a letter: ").lower()
+print(guess)
