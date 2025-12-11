c17e2c0208d3b1765b56c83b9e5d377a81c0011d
nalouddin <nalouddin@gmail.com>
2025-12-11 10:36:47 +0500

    c_m_l xolatga keltirdik

--- app.py
+++ app.py
@@ -11,14 +11,25 @@ for letter in range(len(chosen_word)):
     placeholder += "_"
 print(placeholder)
 
-guess = input("Guess a letter: ").lower()
+game_over = False
+correct_letters = []
+
+while not game_over:
+    guess = input("Guess a letter: ").lower()
 
 
-display = ""
+    display = ""
 
-for letter in chosen_word:
-    if letter == guess:
-        display += letter
-    else:
-        display += "_"
-print(display)
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+    print(display)
+
+    if "_" not in display:
+        game_over = True
+        print("You win!")
