2deb208de8b093fc03e75629a4e52232a7c6a486
nalouddin <nalouddin@gmail.com>
2025-12-11 10:23:23 +0500

    sikil operatir yordamida tanlangan so'zlar siniga qarab _ qo'ish

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
+placeholder = ""
+for letter in range(len(chosen_word)):
+    placeholder += "_"
+print(placeholder)
 guess = input("Guess a letter: ").lower()
 print(guess)
 
